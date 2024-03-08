cc54adce6048ff471c258688a6f573b9da2a8efc
hashlol <muhshim683@gmail.com>
2024-03-08 12:47:01 -0500

    changed folder name

--- Prog 14_01/src/Prog18_01/GraphInterface.java
+++
@@ -1,9 +0,0 @@
-package Prog18_01;
-
-public interface GraphInterface
-{
-    public void addEdge(int v, int w);
-    public void removeEdge(int v, int w);
-    public int[] findAdjacencyVertices(int v);
-    public String toString();
-}
